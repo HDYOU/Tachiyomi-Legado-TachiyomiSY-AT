15e0d4aa808365e49bb6dfe9ac10c9551365712b
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-08-28 16:41:44 +0800

    fix

--- data/self-sh/010-fix-pack-error.sh
+++ data/self-sh/010-fix-pack-error.sh
@@ -10,6 +10,6 @@ echo "run path: $PWD"
 
 ls -lh
 echo "fix Unresolved reference"
-sed -i '/implementation(projects.i18n)/a \    implementation('org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1')' app/build.gradle.kts
+sed -i '/implementation(projects.i18n)/a \    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")' app/build.gradle.kts
 
 exit 0
