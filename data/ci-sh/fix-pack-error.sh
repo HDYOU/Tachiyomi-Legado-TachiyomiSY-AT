30aef209a6bf50c0adf88500d8c7619806fd09ff
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-08-28 16:26:42 +0800

    fix Unresolved reference

---
+++ data/ci-sh/fix-pack-error.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# 出错退出
+# set -e
+
+export HOME="$(cd "`dirname "$0"`"/..; pwd)"
+
+echo "run path: $PWD"
+
+
+ls -lh
+echo "fix Unresolved reference"
+sed -i '/implementation(projects.i18n)/a \    implementation('org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1')' app/build.gradle.kts
+
+exit 0
